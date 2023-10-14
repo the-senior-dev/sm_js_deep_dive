c14997939f0aa65d4f00f966b073061997831f93
Bogdan Nedelcu <bogdanned32@gmail.com>
2023-10-14 16:24:38 +0200

    feat: updated hoisting

--- 03_function_hoisting.js
+++ 03_function_hoisting.js
@@ -1,15 +1,25 @@
-"use strict";
-
+// 🚀 Main Function: Determines what message to show
 function reply(action) {
-  if (action === "greet") return sayHello("foo");
-  return "bye ... !";
+  // 📦 Hoisting in Action: sayHello is accessible here even before its actual definition
+  if (action === "greet") return sayHello("foo");  
+  return `bye ... ${additionalText}!`;  // ❗ Error: additionalText is not defined
 }
+
+// 📦 Let and Const Hoisting: Initialized but not assigned
+let additionalText;  
+const additionalConstText = "read me";  // Const requires immediate initialization
 
 {
-  // this scope
+  // 👋 Helper Function: Greets the user
+  // 📜 Defined Later: But thanks to hoisting, it's like it was always there!
   function sayHello(username) {
-    return `$Hi, I am ${username}!`;
+    return `Hi, I am ${username}!`;
   }
+  
+  // 🚀 Set Let and Const: Now they're available for use
+  additionalText = "see you again"; 
 }
 
-reply("greet");
+// Run reply function
+console.log(reply("greet"));  // Output: "Hi, I am foo!"
+
